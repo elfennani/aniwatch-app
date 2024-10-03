0888d2d9053f2ee2d9fd7c0573f126d421095a60
Nizar Elfennani <elfennani2002@gmail.com>
2024-10-03 18:54:55 +0100

    Fixed video playback
    (cherry picked from commit 7bef9aac70b485a9e701a30f84a0045e7f4746e9)

--- app/src/main/java/com/elfennani/aniwatch/data/repository/EpisodeRepositoryImpl.kt
+++ app/src/main/java/com/elfennani/aniwatch/data/repository/EpisodeRepositoryImpl.kt
@@ -109,17 +109,14 @@ class EpisodeRepositoryImpl @Inject constructor(
 
     private suspend fun fetchAllAnimeShow(showId: Int): Pair<ApolloResponse<ShowStreamingEpisodeQuery.Data>, SearchQuery.Edge?> {
         val show = aniListApolloClient.query(ShowStreamingEpisodeQuery(showId)).execute()
-        val showTitle = when(showId){
-            19 -> "Monster (2004)"
-            else -> show.data?.media?.title?.userPreferred!!
-        }
+        val showTitle = show.data?.media?.title?.userPreferred!!
 
         val searchResponse = retry {
             allAnimeApolloClient.query(SearchQuery(showTitle)).execute()
         }
 
         val allAnimeShow = searchResponse.data?.shows?.edges?.find {
-            (it.aniListId as String?)?.toInt() == showId
+            (it.aniListId as String).toInt() == showId
         }
 
         return Pair(show, allAnimeShow)
